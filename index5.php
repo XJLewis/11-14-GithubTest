47162b5f1e1c767c667f56ade3c2fe6f5c8f8a5b
Xander Lewis <70913145+XJLewis@users.noreply.github.com>
2024-11-14 13:20:12 -0500

    user&pass

--- index5.php
+++ index5.php
@@ -12,8 +12,8 @@ if (!is_logged_in()) {
 
 $host = 'localhost'; 
 $dbname = 'books'; 
-$user = 'mark'; 
-$pass = 'mark';
+$user = 'xander'; 
+$pass = 'passwd';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
